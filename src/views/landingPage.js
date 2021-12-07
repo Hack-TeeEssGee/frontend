6444e23048d0cc6027ceae0cc7470640d85081a4
chirag828049 <cghosh828049@gmail.com>
2021-12-07 11:41:39 +0530

    initialised landingpage;

---
+++ src/views/landingPage.js
@@ -0,0 +1,17 @@
+const LandingPage = () => {
+  return (
+    <div className="landing-page">
+      <div className="hero">
+        <div className="hero-title">KGPverse</div>
+        <div className="hero-subtitle">
+          One place for all the academic, extracurricular and co-curricular
+          information.
+          <br />
+          Made by the KGPians for the KGPians
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LandingPage;
