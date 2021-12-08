f53396e872a468fd0e2516ed2950216eda6fafe2
chirag828049 <cghosh828049@gmail.com>
2021-12-08 08:30:32 +0530

    made navbar responsive;

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,9 +1,21 @@
+import { useState } from "react";
 import tsgLogo from "../assets/tsg-logo.png";
+import UilBars from "@iconscout/react-unicons/icons/uil-bars";
+import UilTimes from "@iconscout/react-unicons/icons/uil-times";
 
 const Navbar = () => {
+  const [isMobNavbarOpen, setIsMobNavbarOpen] = useState(false);
+
   return (
-    <div className="navbar">
-      <div>
+    <div className="navbar" isOpen={String(isMobNavbarOpen)}>
+      <div
+        className="hamburger"
+        onClick={() => setIsMobNavbarOpen(!isMobNavbarOpen)}
+      >
+        {isMobNavbarOpen ? <UilTimes /> : <UilBars />}
+      </div>
+
+      <div className="navbar-logo">
         <img src={tsgLogo} alt="tsg-logo"></img>
       </div>
 
