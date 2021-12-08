2883bcc12d29a700312851705d57f1ef4e779b5c
chirag828049 <cghosh828049@gmail.com>
2021-12-08 22:55:23 +0530

    Added login cards;

--- src/components/landing/loginPortal.js
+++ src/components/landing/loginPortal.js
@@ -1,7 +1,27 @@
+import UilUser from "@iconscout/react-unicons/icons/uil-user";
+
 const LoginPortal = () => {
   return (
     <div className="login-portal">
       <h1>Login Portal</h1>
+      <div className="login-cards-wrapper">
+        <div className="login-card" color="green">
+          <UilUser size={100} />
+          <button className="login-card-button">TSG Official</button>
+        </div>
+        <div className="login-card" color="orange">
+          <UilUser size={100} />
+          <button className="login-card-button">Admin</button>
+        </div>
+        <div className="login-card" color="pink">
+          <UilUser size={100} />
+          <button className="login-card-button">Society</button>
+        </div>
+        <div className="login-card" color="blue">
+          <UilUser size={100} />
+          <button className="login-card-button">Student</button>
+        </div>
+      </div>
     </div>
   );
 };
