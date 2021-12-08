ec89f64cc1006818bd3774425290822ff59a2ea5
chirag828049 <cghosh828049@gmail.com>
2021-12-08 17:19:03 +0530

    Added calendar;

--- src/components/landing/eventCalendar.js
+++ src/components/landing/eventCalendar.js
@@ -1,7 +1,17 @@
+import { useState } from "react";
+import Calendar from "react-calendar";
+
 const EventCalendar = () => {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
   return (
     <div className="event-calendar">
       <h1>Event Calendar</h1>
+      <Calendar
+        onChange={setSelectedDate}
+        value={selectedDate}
+        className="calendar-wrapper"
+      />
     </div>
   );
 };
