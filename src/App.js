14c97b6d51a33d3a29d0544f41d305c96766ac7f
chirag828049 <cghosh828049@gmail.com>
2021-12-09 14:17:06 +0530

    Added login route;

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./views/landingPage";
+import LoginPage from "./views/loginPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/login/" element={<LoginPage />} />
         <Route path="/" element={<LandingPage />} />
       </Routes>
     </BrowserRouter>
---
+++ src/views/loginPage.js
@@ -0,0 +1,10 @@
+const LoginPage = () => {
+
+    return (
+        <div className="login-page">
+            This is login page
+        </div>
+    )
+}
+
+export default LoginPage;
