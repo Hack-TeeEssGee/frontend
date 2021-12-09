198329da5f37d925ca2a0f07f37542f558818ca1
chirag828049 <cghosh828049@gmail.com>
2021-12-09 14:22:47 +0530

    Added URL params;

--- src/views/loginPage.js
+++ src/views/loginPage.js
@@ -1,8 +1,19 @@
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+
 const LoginPage = () => {
+
+    const location = useLocation();
+    const [role, setRole] = useState("");
+
+    useEffect(() => {
+        const urlParams = new URLSearchParams(location.search);
+        setRole(urlParams.get("role"));
+    }, []);
 
     return (
         <div className="login-page">
-            This is login page
+            This is {role} login page.
         </div>
     )
 }
