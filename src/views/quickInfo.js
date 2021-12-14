7ac211db4b5c6b1c71b2b7d91fa49b3200a80077
chirag828049 <cghosh828049@gmail.com>
2021-12-14 23:40:03 +0530

    Modified styling;

--- src/views/quickInfo.js
+++ src/views/quickInfo.js
@@ -2,16 +2,16 @@ const QuickInfo = () => {
 
     return (
 
-        <div className="quick-info">Ha bhai bharosa rakh
+        <div className="quick-info">
 
-            <h1>QUICK INFO</h1>;
+            <button type="button" class="return-btn">Go Back</button>
+            <h1>QUICK INFO</h1>
             <button type="button" class="block">Archive-Statistics of CDC</button>
             <button type="button" class="block">Quick Links</button>
             <button type="button" class="block">Contacts</button>
             <button type="button" class="block">Hall of Residence</button>
             <button type="button" class="block">Societies</button>
             <button type="button" class="block">Faculty</button>
-            <button type="button" class="button1">Go Back</button>
 
         </div >
 
