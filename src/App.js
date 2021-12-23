017a341cf4fb08628f4e2f445d2ee9c4f3a28fdd
chirag828049 <cghosh828049@gmail.com>
2021-12-23 12:50:26 +0530

    Added supertokens init;

--- src/App.js
+++ src/App.js
@@ -6,6 +6,22 @@ import QuickInfo from "./views/quickInfo";
 import SocietyPoint from "./views/societyPoint";
 import StudentProfile from "./views/studentProfile";
 
+import SuperTokens from "supertokens-auth-react";
+import Session from "supertokens-auth-react/recipe/session";
+import { BACKEND_URL, FRONTEND_URL } from "../constants";
+
+SuperTokens.init({
+  appInfo: {
+      // learn more about this on https://supertokens.io/docs/session/appinfo
+      appName: "kgpverse",
+      apiDomain: BACKEND_URL,
+      websiteDomain: FRONTEND_URL
+  },
+  recipeList: [
+      Session.init()
+  ]
+});
+
 function App() {
   return (
     <BrowserRouter>
