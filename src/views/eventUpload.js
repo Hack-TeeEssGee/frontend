c2be2ecd6553640cd13de2fb93a714ba5c04dfa9
Ananya <dasananya8303@gmail.com>
2021-12-31 12:16:38 +0530

    Added Events Uploader;

--- src/views/eventUpload.js
+++ src/views/eventUpload.js
@@ -1,7 +1,19 @@
 const EventUpload = () => {
 
     return (
-        <div className="event-upload">This is event upload page</div>
+        <div className="event-upload">
+            <input type="text" className="type-1" placeholder="Enter name of event" >
+            </input>
+            <input type="text" className="type-1" placeholder="Enter Link of the Event" >
+            </input>
+            <input type="date" className="date-picker" >
+            </input>
+            <label htmlFor="file-upload" className="custom-file-upload">
+                &#8613; &nbsp; Upload Poster of the Event
+            </label>
+            <input id="file-upload" type="file" />
+            <button className="add-button">Add +</button>
+        </div>
     )
 }
 
