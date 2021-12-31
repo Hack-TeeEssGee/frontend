b615de906078417f90c15d93ad1d50a5db7e55c3
chirag828049 <cghosh828049@gmail.com>
2021-12-31 12:21:19 +0530

    Added events page template;

--- src/views/eventsPage.js
+++ src/views/eventsPage.js
@@ -1,7 +1,87 @@
-const EventsPage = () => {
+import Dashboard from "../components/dashboard";
+import { UilGameStructure, UilMusic, UilSetting, UilVolleyball, UilUsersAlt, UilInfoCircle } from '@iconscout/react-unicons';
+import { useState } from "react";
+
+const RecentEvents = (props) => {
 
     return (
-        <div className="events-page">This is events page.</div>
+        <div className="recent-events">
+        </div>
+    )
+}
+
+const EventResults = () => {
+
+    return (
+        <div className="event-results">
+        </div>
+    )
+}
+
+const EventsPage = () => {
+
+    const dashboardHeaderData = {
+        title: "TSG Events",
+        subtitle: "",
+        icon: UilGameStructure
+    }
+
+    const dashboardListData = {
+        defaultSelection: 0,
+        specialSelection: 4, //Enter -1 for none
+        listData: [
+            {
+                option: "Technology",
+                icon: UilSetting
+            },
+            {
+                option: "Social and Cultural",
+                icon: UilMusic
+            },
+            {
+                option: "Sports and Games",
+                icon: UilVolleyball
+            },
+            {
+                option: "Students' Welfare",
+                icon: UilUsersAlt
+            },
+            {
+                option: "Results",
+                icon: UilInfoCircle
+            }
+        ]
+    }
+
+    const [currentSelection, setCurrentSelection] = useState(dashboardListData.defaultSelection);
+
+    var title, BodyContent;
+
+    if (currentSelection >= 0 && currentSelection <= 3) {
+        title = "RECENT EVENTS";
+        BodyContent = RecentEvents;
+    }
+    else {
+        title = "RESULTS";
+        BodyContent = EventResults;
+    }
+
+    return (
+        <div className="events-page">
+            <Dashboard
+                headerData={dashboardHeaderData}
+                listData={dashboardListData}
+                currentSelection={currentSelection}
+                changeSelection={setCurrentSelection}
+            />
+            <div className="container">
+                <button onClick={() => window.location.href = `${window.location.origin}/`}>HOME</button>
+                <div className="title">{title}</div>
+                <BodyContent
+                    mode={dashboardListData.listData[currentSelection].option}
+                />
+            </div>
+        </div>
     )
 }
 
