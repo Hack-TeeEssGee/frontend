141e1e1ddebe00b00d1b8fb22d374a9c9b9e3f75
Ashwani <rajivkamal.im421@gmail.com>
2022-01-23 02:37:25 +0530

    add props in contacts

--- src/components/contactCard.js
+++ src/components/contactCard.js
@@ -4,9 +4,9 @@ const ContactCard = (props) => {
 
     return (
         <div className="contact-card">
-            <img src={props.photo} alt={props.name}></img>
-            <div className="contact-name">{props.name}</div>
-            <div className="contact-position">{props.position}</div>
+            <img src={props.image} alt={props.Name}></img>
+            <div className="contact-name">{props.Name}</div>
+            <div className="contact-position">{props.Position}</div>
             <div className="contact-links">
                 <a href={`mailto:${props.email}`}><UilEnvelope color="orangered" size={40} /></a>
                 <a href={`Tel:${props.phone}`}><UilPhone color="purple" size={40} /></a>
