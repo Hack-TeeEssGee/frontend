4d4068f326b26035806df75b4be5e12897e30932
Ashwani <rajivkamal.im421@gmail.com>
2022-01-23 22:15:10 +0530

    add regex for verify email

--- src/utils/verifyEmail.js
+++ src/utils/verifyEmail.js
@@ -2,7 +2,10 @@ const verifyEmail = (email) => {
 
     //email address should belong to iitkgp.
     //use regex to check for a valid email
-    return true;
+    const EMAIL_VALIDATOR = /^.*@(kgpian.)?iitkgp.ac.in$/i;
+    const res = email.match(EMAIL_VALIDATOR);
+    if(res == null) return false;
+    else return true;
 }
 
 export default verifyEmail;
