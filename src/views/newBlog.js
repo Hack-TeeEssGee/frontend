f98446c015b5e9a78fd5c7f3d25cc7cc488771d8
chirag828049 <cghosh828049@gmail.com>
2022-01-23 22:14:26 +0530

    Fixed blog routing;

--- src/views/newBlog.js
+++ src/views/newBlog.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Editor from "rich-markdown-editor";
 import myTheme from "../utils/markdownTheme";
 import { useNavigate } from "react-router-dom";
+import { SessionAuth } from "supertokens-auth-react/recipe/session";
 
 const NewBlog = () => {
 
@@ -18,19 +19,21 @@ const NewBlog = () => {
     }
 
     return (
-        <div className="new-blog">
-            <div className="nav-button-wrapper">
-                    <button className="button" onClick={() => navigate("/blog")}>GO BACK</button>
+        <SessionAuth requireAuth={true} redirectToLogin={() => { navigate("/login?role=student") }}>
+            <div className="new-blog">
+                <div className="nav-button-wrapper">
+                        <button className="button" onClick={() => navigate("/blog")}>GO BACK</button>
+                </div>
+                <input type="text" value={title} onChange={(event) => setTitle(event.target.value)} placeholder="Enter a title for your blog"></input>
+                <div className="new-blog-editor">
+                    <Editor
+                        theme={myTheme}
+                        onChange={(changeFunc) => setMarkdownContent(changeFunc)}
+                    />
+                </div>
+                <button className="button" onClick={() => submitBlog()}>Submit</button>
             </div>
-            <input type="text" value={title} onChange={(event) => setTitle(event.target.value)} placeholder="Enter a title for your blog"></input>
-            <div className="new-blog-editor">
-                <Editor
-                    theme={myTheme}
-                    onChange={(changeFunc) => setMarkdownContent(changeFunc)}
-                />
-            </div>
-            <button className="button" onClick={() => submitBlog()}>Submit</button>
-        </div>
+        </SessionAuth>
     )
 }
 
